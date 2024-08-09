fdc97feac3334d77ccafbcb2e6e55d0396ec9a54
jbravo108 <154507707+jbravo108@users.noreply.github.com>
2024-08-09 12:51:17 +0200

    Add files via upload

---
+++ ejer01/index.js
@@ -0,0 +1,17 @@
+//ejercicio 1.1
+console.log(document.querySelector('.showme'))
+
+//ejercicio 1.2
+console.log(document.querySelector('#pillado'))
+
+//ejercicio 1.3
+console.log(document.querySelectorAll('p'))
+
+//ejercicio 1.4
+console.log(document.querySelectorAll('.pokemon'))
+
+//ejercicio 1.5
+console.log(document.querySelectorAll("[data-function='testMe']"))
+
+//ejercicio 1.6
+console.log(document.querySelectorAll("[data-function='testMe']")[2])
